96beb2d7bc2cb5d88ec7e0a0576e349347069809
giovannilobianco <giovanni.lo-bianco@imt-atlantique.fr>
2018-10-24 15:33:26 +0200

    Fixes some bug

--- src/main/java/org/chocosolver/solver/search/strategy/countingbased/CountingBasedStrategy.java
+++ src/main/java/org/chocosolver/solver/search/strategy/countingbased/CountingBasedStrategy.java
@@ -2,10 +2,12 @@ package org.chocosolver.solver.search.strategy.countingbased;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import org.chocosolver.solver.Model;
 import org.chocosolver.solver.constraints.Constraint;
 import org.chocosolver.solver.constraints.Propagator;
+import org.chocosolver.solver.search.strategy.assignments.DecisionOperator;
 import org.chocosolver.solver.search.strategy.assignments.DecisionOperatorFactory;
 import org.chocosolver.solver.search.strategy.countingbased.tools.CountingTools;
 import org.chocosolver.solver.search.strategy.countingbased.tools.IntVarAssignment;
@@ -77,6 +79,8 @@ public abstract class CountingBasedStrategy extends AbstractStrategy<IntVar> {
 	private String estimatorAlldifferent = CountingEstimators.ALLDIFFERENT_PQZ;
 	private String estimatorGCC = CountingEstimators.GCC_PQZ;
 
+	private final IntDecision WRONG;
+
 	// ***********************************************************************************
 	// CONSTRUCTORS
 	// ***********************************************************************************
@@ -105,13 +109,14 @@ public abstract class CountingBasedStrategy extends AbstractStrategy<IntVar> {
 				}
 		}
 		this.countables = new Countable[countableList.size()];
-		for(int k=0; k<countableList.size(); k++){
-			this.countables[k]=countableList.get(k);
+		for (int k = 0; k < countableList.size(); k++) {
+			this.countables[k] = countableList.get(k);
 		}
-		
-		
 
 		this.tools = new CountingTools();
+		
+		WRONG = new IntDecision(pool);
+		WRONG.set(model.intVar(0), 1, DecisionOperatorFactory.makeIntEq());
 	}
 
 	// ***********************************************************************************
@@ -120,55 +125,81 @@ public abstract class CountingBasedStrategy extends AbstractStrategy<IntVar> {
 
 	public Decision<IntVar> getDecision() {
 
+		// If it is the first decision we make or if the order list need to
+		// be updated, then we compute it
+		if (order == null || needUpdate()) {
+			computeOrder();
+			if (order == null) {
+				// This decision is wrong and cannot be refutable
+				WRONG.rewind();
+				WRONG.setRefutable(false);
+				return WRONG;
+			}
+			next = 0;
+		}
+
+		// We need to save the state of "next" and "order" in order to
+		// retrieve it when we will backtrack
+		int nextSave = next;
+		IntVarAssignment[] orderSave = order;
+
+		// Otherwise, we look for the next possible assignment in order. As
+		// we do not recompute the order array at each decision, some
+		// assignments might be impossible at this stage.
+		int k = next;
+		while (k < order.length
+				&& (order[k].getVar().isInstantiated() || !order[k].getVar().contains(order[k].getVal()))) {
+			k++;
+		}
+		next = k;
+
+		String s = "";
+		for (int kk = Math.max(0, next-1); kk < (order.length > next + 3 ? next + 3 : order.length); kk++) {
+			s += order[kk] + ", ";
+		}
+		System.out.println(s);
+		System.out.println("Next : " + next);
+
 		// Initializing the decision pool
 		IntDecision d = pool.getE();
 		if (d == null)
 			d = new IntDecision(pool);
 
-		// If it is the first decision we make or if the order list need to
-		// be updated, then we compute it
-		if (order == null || needUpdate()) {
-			computeOrder();
-			next = 0;
-		} else {
-			// Otherwise, we look for the next possible assignment in order. As
-			// we do not recompute the order array at each decision, some
-			// assignments might be impossible at this stage.
-			int k = next;
-			while (k < order.length
-					&& (order[k].getVar().isInstantiated() || !order[k].getVar().contains(order[k].getVal()))) {
-				k++;
-			}
-			next = k;
-			if (next == order.length) {
-				// If there remains no assignment, it means that every variable
-				// that is contained in countable constraints is already
-				// instantiated. Then we apply a default strategy to choose the
-				// next assignment for the variables that we did not consider
-				// yet.
-				for (IntVar v : this.getVariables()) {
-					if (!v.isInstantiated()) {
-						d.set(v, v.getLB(), DecisionOperatorFactory.makeIntEq());
-						return d;
-					}
+		if (next == order.length) {
+			// If there remains no assignment, it means that every variable
+			// that is contained in countable constraints is already
+			// instantiated. Then we apply a default strategy to choose the
+			// next assignment for the variables that we did not consider
+			// yet.
+			for (IntVar v : this.getVariables()) {
+				if (!v.isInstantiated()) {
+					d.set(v, v.getLB(), DecisionOperatorFactory.makeIntEq());
+					return d;
 				}
-			} else {
-				// Otherwise, we found the next decision
-				IntVarAssignment nextAssignment = order[next];
-				d.set(nextAssignment.getVar(), nextAssignment.getVal(), DecisionOperatorFactory.makeIntEq());
-
-				// We need to save the state of "next" and "order" in order to
-				// retrieve it when we will backtrack
-				int nextSave = next;
-				IntVarAssignment[] orderSave = order;
-
-				model.getEnvironment().save(() -> {
-					this.next = nextSave;
-					this.order = orderSave;
-				});
 			}
+			// TODO : renvoyer null et faire un séquenceur
+
+		} else {
+			// Otherwise, we find the next decision
+			IntVarAssignment nextAssignment = order[next];
+			d.set(nextAssignment.getVar(), nextAssignment.getVal(), DecisionOperatorFactory.makeIntEq());
+			System.out.println("Decision : " + nextAssignment.getVar() + " " + nextAssignment.getVal());
+			System.out.println("--------------------------------");
+
+			model.getEnvironment().save(() -> {
+				this.next = nextSave;
+				this.order = orderSave;
+
+				System.out.println("BAAAAAAACKTRAAAAAACK !!");
+				System.out.println("Next : " + next);
+				System.out.println("--------------------------------");
+			});
+			return d;
 		}
-		return d;
+
+		// If we get here, then every variables are instantiated, then we return
+		// null
+		return null;
 	}
 
 	abstract public void computeOrder();
@@ -178,6 +209,10 @@ public abstract class CountingBasedStrategy extends AbstractStrategy<IntVar> {
 	 * @return true if the order list need to be updated
 	 */
 	public boolean needUpdate() {
+		/*
+		 * Random rnd = new Random(); double p = rnd.nextDouble(); if (p < 0.1)
+		 * { System.out.println("Needs update"); } return p < 0.1;
+		 */
 		return false;
 	}
 
@@ -188,13 +223,13 @@ public abstract class CountingBasedStrategy extends AbstractStrategy<IntVar> {
 	public Countable[] getCountables() {
 		return countables;
 	}
-	
-	public void setEstimatorAlldifferent(String estimator){
-		this.estimatorAlldifferent=estimator;
+
+	public void setEstimatorAlldifferent(String estimator) {
+		this.estimatorAlldifferent = estimator;
 	}
-	
-	public void setEstimatorGCC(String estimator){
-		this.estimatorGCC=estimator;
+
+	public void setEstimatorGCC(String estimator) {
+		this.estimatorGCC = estimator;
 	}
 
 	public String getEstimatorAlldifferent() {
@@ -208,7 +243,5 @@ public abstract class CountingBasedStrategy extends AbstractStrategy<IntVar> {
 	public CountingTools getTools() {
 		return tools;
 	}
-	
-	
 
 }
